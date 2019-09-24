845753b692d39cab584539bcdb1377b8c2830113
musamuss <45773032+musamuss@users.noreply.github.com>
2019-09-24 14:12:14 +0300

    Initial Commit

---
+++ testTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testTask
+//
+//  Created by admin on 24/09/2019.
+//  Copyright © 2019 musamuss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
